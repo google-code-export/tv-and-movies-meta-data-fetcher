13b27c4fddeef537e0e205a2da06386f2f52de64
jp.stanford <jp.stanford@localhost>
2008-11-12 17:33:34 +0000

    Added missing tests

---
+++ tests/org/stanwood/tv/FileHelper.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright (C) 2008  John-Paul.Stanford <dev@stanwood.org.uk>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.stanwood.tv;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class FileHelper {
+	public static File createTmpDir() throws IOException {
+		File dir = File.createTempFile("show", "");
+		dir.delete();
+		dir.mkdir();
+		return dir;
+	}
+
+	public static void copy(File src, File dst) throws IOException {
+		InputStream in = new FileInputStream(src);
+		OutputStream out = new FileOutputStream(dst);
+
+		// Transfer bytes from in to out
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
+	}
+
+	public static void copy(InputStream in, File dst) throws IOException {		
+		OutputStream out = new FileOutputStream(dst);
+
+		// Transfer bytes from in to out
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
+	}
+
+	public static boolean deleteDir(File dir) {
+		if (dir.isDirectory()) {
+			String[] children = dir.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean success = deleteDir(new File(dir, children[i]));
+				if (!success) {
+					return false;
+				}
+			}
+		}
+
+		// The directory is now empty so delete it
+		return dir.delete();
+	}
+	
+	public static void displayFile(File file,PrintStream os) throws IOException {	   
+	        BufferedReader in = new BufferedReader(new FileReader(file));
+	        String str;
+	        while ((str = in.readLine()) != null) {
+	        	os.println(str);
+	        }
+	        in.close();	  
+	}
+}
