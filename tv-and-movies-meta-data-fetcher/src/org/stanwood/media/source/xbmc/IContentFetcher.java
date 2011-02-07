a1352e36e164e8ca2a916a6e0cde19fde8fc141f
jp.stanford <jp.stanford@localhost>
2011-02-07 22:35:34 +0000

    Updated java doc

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/source/xbmc/IContentFetcher.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/source/xbmc/IContentFetcher.java
@@ -6,7 +6,17 @@ import java.net.URL;
 
 import org.stanwood.media.source.SourceException;
 
+/**
+ * This interface should be implemented by classes used to fetch content from a URL
+ */
 public interface IContentFetcher {
 
+	/**
+	 * This method should be implemented to fetch content from a URL
+	 * @param url The URL
+	 * @return A input stream to the content
+	 * @throws IOException Thrown if their is a problem with I/O
+	 * @throws SourceException Thrown if their are any other problems
+	 */
 	public InputStream getStreamToURL(URL url) throws IOException, SourceException;
 }
