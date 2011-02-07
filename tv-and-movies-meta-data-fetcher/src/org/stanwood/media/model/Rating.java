435749183dd6e134a5aa849d97f63b9d7c82229c
jp.stanford <jp.stanford@localhost>
2011-02-07 22:39:00 +0000

    Updated java doc

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/model/Rating.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/model/Rating.java
@@ -1,10 +1,18 @@
 package org.stanwood.media.model;
 
+/**
+ * Used to store information on video ratings
+ */
 public class Rating {
 
 	private float rating;
 	private int numberOfVotes;
 
+	/**
+	 * The constructor used to create a instance of the rating class
+	 * @param rating The value of the rating
+	 * @param numberOfVotes The number of votes the rating got
+	 */
 	public Rating(float rating, int numberOfVotes) {
 		super();
 		this.rating = rating;
