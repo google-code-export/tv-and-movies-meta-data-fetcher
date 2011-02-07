a4f1c5182f7225d49110118eb200b3bfbb054bdc
jp.stanford <jp.stanford@localhost>
2011-02-07 22:37:56 +0000

    Updated java doc

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/model/Actor.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/model/Actor.java
@@ -1,20 +1,36 @@
 package org.stanwood.media.model;
 
+/**
+ * Used to store information on actors
+ */
 public class Actor {
 
 	private String name;
 	private String role;
 
+	/**
+	 * The constructor used to create a instance of the class
+	 * @param name The name of the actor
+	 * @param role The role the actor played
+	 */
 	public Actor(String name, String role) {
 		super();
 		this.name = name;
 		this.role = role;
 	}
 
+	/**
+	 * Used to get the name of the actor
+	 * @return The name of the actor
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Used to get the role the actor played
+	 * @return the role the actor played
+	 */
 	public String getRole() {
 		return role;
 	}
