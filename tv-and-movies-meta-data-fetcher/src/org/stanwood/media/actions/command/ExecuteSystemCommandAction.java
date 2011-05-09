9d98b3564c34ef1d3637e60d2e11c9dcb3d8dcda
jp.stanford <jp.stanford@localhost>
2011-05-09 12:25:44 +0000

    Add support for deleting or creating new files

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/actions/command/ExecuteSystemCommandAction.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/actions/command/ExecuteSystemCommandAction.java
@@ -18,6 +18,10 @@ public class ExecuteSystemCommandAction extends AbstractAction {
 
 	private String PARAM_CMD_KEY = "command";
 	private String cmd;
+	private Object PARAM_NEW_FILE_KEY;
+	private Object PARAM_DELETED_FILE_KEY;
+	private String newFile;
+	private String deletedFile;
 
 	@Override
 	public void perform(MediaDirectory dir, File file,IActionEventHandler actionEventHandler) throws ActionException {
@@ -32,11 +36,20 @@ public class ExecuteSystemCommandAction extends AbstractAction {
 			} catch (IOException e) {
 				throw new ActionException("Unable to execute system command: " + cmdLine.toString());
 			}
+			if (deletedFile!=null) {
+				actionEventHandler.sendEventDeletedFile(new File(deletedFile));
+			}
+			if (newFile!=null) {
+				actionEventHandler.sendEventNewFile(new File(newFile));
+			}
 		}
 	}
 
 	private String replaceVars(String cmd,File mediaFile) {
-		return cmd.replaceAll("\\$MEDIAFILE", mediaFile.getAbsolutePath());
+		String s = cmd.replaceAll("\\$MEDIAFILE", mediaFile.getAbsolutePath());
+		s = cmd.replaceAll("\\$NEWFILE", newFile);
+		s = cmd.replaceAll("\\$DELETEDFILE", deletedFile);
+		return s;
 	}
 
 	@Override
@@ -44,7 +57,15 @@ public class ExecuteSystemCommandAction extends AbstractAction {
 		if (key.equals(PARAM_CMD_KEY)) {
 			this.cmd = value;
 		}
-		throw new ActionException("Unsupported parameter for action '"+key+"'");
+		else if (key.equals(PARAM_NEW_FILE_KEY)) {
+			this.newFile = value;
+		}
+		else if (key.equals(PARAM_DELETED_FILE_KEY)) {
+			this.deletedFile = value;
+		}
+		else {
+			throw new ActionException("Unsupported parameter for action '"+key+"'");
+		}
 	}
 
 }
