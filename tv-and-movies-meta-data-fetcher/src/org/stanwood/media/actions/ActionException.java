a393c57a82d0ca7d30de6f698ef62975a7399c55
jp.stanford <jp.stanford@localhost>
2011-05-16 14:47:38 +0000

    Updated more java doc

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/actions/ActionException.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/actions/ActionException.java
@@ -8,25 +8,54 @@ import org.stanwood.media.logging.StanwoodException;
 public class ActionException extends StanwoodException {
 
 
-	/** {@inheritDoc} */
+	/**
+     * Constructs a new exception with <code>null</code> as its detail message.
+     * The cause is not initialised, and may subsequently be initialised by a
+     * call to {@link #initCause}.
+     */
 	public ActionException() {
 		super();
 	}
 
-	/** {@inheritDoc} */
+	/**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * <code>cause</code> is <i>not</i> automatically incorporated in
+     * this exception's detail message.
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method).
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
 	public ActionException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
-	/** {@inheritDoc} */
+	/**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialised, and may subsequently be initialised by
+     * a call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
 	public ActionException(String message) {
 		super(message);
 	}
 
-	/** {@inheritDoc} */
+	/**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialised, and may subsequently be initialised by
+     * a call to {@link #initCause}.
+     *
+     * @param   cause   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
 	public ActionException(Throwable cause) {
 		super(cause);
 	}
-
 
 }
