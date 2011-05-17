a2098c9a94f45e8cda8b9f768d2706dbf1263602
jp.stanford <jp.stanford@localhost>
2011-05-17 12:17:23 +0000

    Updated java doc

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/actions/podcast/VideoFeedFile.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/actions/podcast/VideoFeedFile.java
@@ -8,6 +8,9 @@ import java.util.Date;
 import org.stanwood.media.model.IVideo;
 import org.stanwood.media.setup.MediaDirConfig;
 
+/**
+ * A vidoe base feed file
+ */
 public class VideoFeedFile implements IFeedFile {
 
 	private File file;
@@ -15,6 +18,15 @@ public class VideoFeedFile implements IFeedFile {
 	private URL url;
 	private String contentType;
 
+	/**
+	 * The constructor
+	 * @param file The location of the file
+	 * @param dirConfig The media directory configuration
+	 * @param media The media file information
+	 * @param baseUrl The base URL of the feed
+	 * @param contentType The content type of the file
+	 * @throws MalformedURLException Thrown if their is a problem create URL's
+	 */
 	public VideoFeedFile(File file,MediaDirConfig dirConfig, IVideo media, String baseUrl,String contentType) throws MalformedURLException {
 		this.file = file;
 		this.media = media;
@@ -27,21 +39,25 @@ public class VideoFeedFile implements IFeedFile {
 		url = new URL(baseUrl+"/"+relPath);
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public String getContentType() {
 		return contentType;
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public File getFile() {
 		return file;
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public Date getLastModified() {
 		return new Date(file.lastModified());
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public String getTitle() {
 		return media.getTitle();
@@ -58,11 +74,13 @@ public class VideoFeedFile implements IFeedFile {
 		}
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public URL getLink() {
 		return url;
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public String getDescription() {
 		return media.getSummary();
