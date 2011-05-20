d885033bd4acf3694eba23e746ab9c0e6cf7475d
jp.stanford <jp.stanford@localhost>
2011-05-20 14:16:35 +0000

    Updated java doc

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/search/EpisodeFileNameStraregy.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/search/EpisodeFileNameStraregy.java
@@ -6,11 +6,15 @@ import java.util.regex.Pattern;
 
 import org.stanwood.media.MediaDirectory;
 
+/**
+ * This searching strategy will search using common show naming conventions
+ */
 public class EpisodeFileNameStraregy implements ISearchStrategy {
 
 	private final static Pattern PATTERN_EP1 = Pattern.compile("(.*?) S\\d+E\\d+ .*",Pattern.CASE_INSENSITIVE);
 	private final static Pattern PATTERN_EP2 = Pattern.compile("(.*?) \\d+x\\d+ .*",Pattern.CASE_INSENSITIVE);
 
+	/** {@inheritDoc} */
 	@Override
 	public SearchDetails getSearch(File mediaFile, File rootMediaDir,String renamePattern, MediaDirectory mediaDir) {
 		StringBuilder term = new StringBuilder(mediaFile.getName());
