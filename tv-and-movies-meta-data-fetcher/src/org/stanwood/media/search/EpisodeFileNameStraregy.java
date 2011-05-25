da22f041681368c035555b1fd3744e4146753cf0
jp.stanford <jp.stanford@localhost>
2011-05-25 13:00:30 +0000

    Improved show searching

--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/search/EpisodeFileNameStraregy.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/search/EpisodeFileNameStraregy.java
@@ -13,7 +13,9 @@ public class EpisodeFileNameStraregy implements ISearchStrategy {
 
 	private final static Pattern PATTERN_EP1 = Pattern.compile("(.*?) S\\d+E\\d+ .*",Pattern.CASE_INSENSITIVE);
 	private final static Pattern PATTERN_EP2 = Pattern.compile("(.*?) \\d+x\\d+ .*",Pattern.CASE_INSENSITIVE);
-	private final static Pattern PATTERN_EP3 = Pattern.compile("(.*?) \\d\\d\\d .*",Pattern.CASE_INSENSITIVE);
+	private final static Pattern PATTERN_EP3 = Pattern.compile("(.*?) S\\d+ E\\d+ .*",Pattern.CASE_INSENSITIVE);
+	private final static Pattern PATTERN_EP4 = Pattern.compile("(.*?) \\d\\d\\d .*",Pattern.CASE_INSENSITIVE);
+
 
 	/** {@inheritDoc} */
 	@Override
@@ -31,6 +33,10 @@ public class EpisodeFileNameStraregy implements ISearchStrategy {
 			return new SearchDetails(m.group(1), null, null);
 		}
 		m = PATTERN_EP3.matcher(term);
+		if (m.matches()) {
+			return new SearchDetails(m.group(1), null, null);
+		}
+		m = PATTERN_EP4.matcher(term);
 		if (m.matches()) {
 			return new SearchDetails(m.group(1), null, null);
 		}
--- tv-and-movies-meta-data-fetcher/src/org/stanwood/media/search/ShowSearcher.java
+++ tv-and-movies-meta-data-fetcher/src/org/stanwood/media/search/ShowSearcher.java
@@ -17,9 +17,6 @@ public abstract class ShowSearcher extends AbstractMediaSearcher {
 	private final static List<ISearchStrategy> strategies = new ArrayList<ISearchStrategy>();
 	private final static ISearchStrategy episodeFileStrategy;
 
-//	private final static Pattern PATTERN_EP1 = Pattern.compile("(.*?)\\.S\\d+E\\d+\\..*",Pattern.CASE_INSENSITIVE);
-//	private final static Pattern PATTERN_EP2 = Pattern.compile("(.*?)\\.\\d+x\\d+\\..*",Pattern.CASE_INSENSITIVE);
-
 	static {
 		strategies.add(new ReversePatternSearchStrategy(Token.SHOW_NAME,true));
 
@@ -42,35 +39,6 @@ public abstract class ShowSearcher extends AbstractMediaSearcher {
 				return null;
 			}
 		});
-
-//		episodeFileStrategy = new ISearchStrategy() {
-//			@Override
-//			public SearchDetails getSearch(File episodeFile, File rootMediaDir, String renamePattern,MediaDirectory mediaDir) {
-//				StringBuilder fileName = new StringBuilder(episodeFile.getName());
-//				Matcher m = PATTERN_EP1.matcher(fileName);
-//				StringBuilder term = null;
-//				if (m.matches()) {
-//					term = new StringBuilder(m.group(1));
-//
-//				}
-//				m = PATTERN_EP2.matcher(fileName);
-//				if (m.matches()) {
-//					term = new StringBuilder(m.group(1));
-//				}
-//
-//				if (term!=null) {
-//					int pos = term.lastIndexOf(" ");
-//					if (pos!=-1) {
-//						term.replace(0, pos+1, "");
-//					}
-//					SearchHelper.replaceWithSpaces(term);
-//					return new SearchDetails(term.toString(),null,1);
-//				}
-//
-//				return null;
-//			}
-//		};
-//		strategies.add(episodeFileStrategy);
 
 		episodeFileStrategy = new EpisodeFileNameStraregy();
 		strategies.add(episodeFileStrategy);
