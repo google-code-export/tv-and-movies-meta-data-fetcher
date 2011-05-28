89cc4806900385adf26972a9a6971154fe6bd1b6
jp.stanford <jp.stanford@localhost>
2011-05-28 09:40:39 +0000

    Added new command "mm-mp4-list-atoms" which will list the atoms a mp4/m4v file.

---
+++ tv-and-movies-meta-data-fetcher/build/installer/scripts/windows/mm-mp4-list-atoms.bat
@@ -0,0 +1,47 @@
+@echo off
+
+REM Set jvm heap initial and maximum sizes (in megabytes).
+set JAVA_HEAP_INIT_SIZE=64
+set JAVA_HEAP_MAX_SIZE=192
+
+REM Find a java installation.
+if not exist JAVA_HOME goto :javaHomeNotSet
+set JAVA=%JAVA_HOME%/bin/java
+
+:doit
+
+REM Main class
+set MAIN=org.stanwood.media.store.mp4.CLIListAtoms
+
+REM Locations of libraries
+set LIB_DIR=..\lib
+
+REM Setup class path
+set CLASSPATH=%LIB_DIR%\MediaManager-1.98.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\antlr-runtime-3.1.3.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-cli-1.1.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-exec-1.1.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-lang-2.6.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-logging-1.0.4.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\isoparser-svn.r114.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\jdom.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\log4j-1.2.15.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\mysql-connector-java-5.1.6.jar
+set CLASSPATH=%CLASSPATH%;%LIB_DIR%\rome-1.0.jar
+
+
+REM Launch app
+call %JAVA% -Xms%JAVA_HEAP_INIT_SIZE%M -Xmx%JAVA_HEAP_MAX_SIZE%M -classpath %CLASSPATH% %MAIN% %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+goto :exit
+
+:javaHomeNotSet
+
+echo "Warning: JAVA_HOME environment variable not set! Consider setting it."
+echo "          Attempting to use java on the path..."
+
+set JAVA=java
+
+goto :doit
+
+:exit
