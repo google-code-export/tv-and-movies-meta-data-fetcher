ef4cdc6f2c21e4fa6bc3b2eb0639abb0279b18cb
jp.stanford <jp.stanford@localhost>
2011-06-01 15:39:56 +0000

    Script now using all libs in dir

--- tv-and-movies-meta-data-fetcher/build/installer/scripts/windows/mm-copy-store-to-store.bat
+++ tv-and-movies-meta-data-fetcher/build/installer/scripts/windows/mm-copy-store-to-store.bat
@@ -17,18 +17,8 @@ REM Locations of libraries
 set LIB_DIR=..\lib
 
 REM Setup class path
-set CLASSPATH=%LIB_DIR%\MediaManager-1.98.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\antlr-runtime-3.1.3.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-cli-1.1.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-exec-1.1.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-lang-2.6.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\commons-logging-1.0.4.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\isoparser-svn.r145.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\jdom.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\log4j-1.2.15.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\mysql-connector-java-5.1.6.jar
-set CLASSPATH=%CLASSPATH%;%LIB_DIR%\rome-1.0.jar
-
+set CLASSPATH=
+for /f %%a IN (‘dir /b %LIB_DIR&/*.jar’) do set CLASSPATH=%CLASSPATH%;%%a 
 
 REM Launch app
 call %JAVA% -Xms%JAVA_HEAP_INIT_SIZE%M -Xmx%JAVA_HEAP_MAX_SIZE%M -classpath %CLASSPATH% %MAIN% %1 %2 %3 %4 %5 %6 %7 %8 %9
