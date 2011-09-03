fbe38be7918ce23d1659ce60e18a421783b4d83a
jp.stanford <jp.stanford@localhost>
2011-09-03 14:49:38 +0000

    Fixed error message text

--- tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/setup/Plugin.java
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/setup/Plugin.java
@@ -39,7 +39,12 @@ public class Plugin {
 	 */
 	@Override
 	public String toString() {
-		return jar+":"+pluginClass; //$NON-NLS-1$
+		if (jar==null) {
+			return "<classpath>:"+pluginClass; //$NON-NLS-1$
+		}
+		else {
+			return jar+":"+pluginClass; //$NON-NLS-1$
+		}
 	}
 
 
