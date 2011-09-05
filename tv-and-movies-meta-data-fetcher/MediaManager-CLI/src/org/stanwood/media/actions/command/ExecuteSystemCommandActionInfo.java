d4c4a3c2bdfd0658f194a282743c94bdadafd1bf
jp.stanford <jp.stanford@localhost>
2011-09-05 09:54:33 +0000

    Fixed warnings

--- tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/actions/command/ExecuteSystemCommandActionInfo.java
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/actions/command/ExecuteSystemCommandActionInfo.java
@@ -25,12 +25,19 @@ import org.stanwood.media.extensions.ParameterType;
  */
 public class ExecuteSystemCommandActionInfo extends ExtensionInfo<ExecuteSystemCommandAction> {
 
+	/** Parameter type information for the parameter that specifies a command to execute on finding acceptable media files*/
 	public final static ParameterType PARAM_CMD_ON_FILE_KEY = new ParameterType("commandOnFile",String.class,false); //$NON-NLS-1$
+	/** Parameter type information for the parameter that specifies a command to execute on finding acceptable directories within the media directory */
 	public final static ParameterType PARAM_CMD_ON_DIR_KEY = new ParameterType("commandOnDirectory",String.class,false); //$NON-NLS-1$
+	/** Parameter type information for the parameter that specifies a comma separated list of media file extensions to accept */
 	public final static ParameterType PARAM_EXTENSIONS_KEY = new ParameterType("extensions",String.class,false); //$NON-NLS-1$
+	/** Parameter type information for the parameter that specifies if this command creates a new file, then the name should be in this parameter */
 	public final static ParameterType PARAM_NEW_FILE_KEY = new ParameterType("newFile",String.class,false); //$NON-NLS-1$
+	/** Parameter type information for the parameter that specifies If this command deletes a new file, then the name should be in this parameter */
 	public final static ParameterType PARAM_DELETED_FILE_KEY = new ParameterType("deletedFile",String.class,false); //$NON-NLS-1$
+	/** Parameter type information for the parameter that specifies The name of a file, that if it exists, then this action will not perform */
 	public final static ParameterType PARAM_ABORT_IF_FILE_EXISTS = new ParameterType("abortIfFileExists",String.class,false); //$NON-NLS-1$
+
 	private final static ParameterType PARAM_TYPES[] = {PARAM_CMD_ON_FILE_KEY,PARAM_CMD_ON_DIR_KEY,PARAM_EXTENSIONS_KEY,PARAM_NEW_FILE_KEY,PARAM_DELETED_FILE_KEY,PARAM_ABORT_IF_FILE_EXISTS};
 
 	/**
