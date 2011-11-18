0394f9061cfa10e4435267aab9e9de908ab83723
jp.stanford <jp.stanford@localhost>
2011-11-18 15:43:51 +0000

    Added more mp4 metadata reading/writing

--- tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/store/mp4/AtomNameLookup.java
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/store/mp4/AtomNameLookup.java
@@ -1,38 +1,18 @@
 package org.stanwood.media.store.mp4;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Used to lookup atom display names from their keys
  */
 public class AtomNameLookup {
 
-	private Map<String,String> names = new HashMap<String,String>();
 
 	/**
 	 * The Constructor
 	 */
 	@SuppressWarnings("nls")
 	public AtomNameLookup() {
-		names.put("stik","Media type");
-		names.put("tven","Episode ID");
-		names.put("tvsh","TV show name");
-		names.put("tvsn","TV season number");
-		names.put("tves","TV episode number");
-		names.put("©day","Release year");
-		names.put("©nam","Title");
-		names.put("desc","Summary");
-		names.put("©gen","Genre");
-		names.put("catg","Category");
-		names.put("gnre","Genre type");
-		names.put("©too","Encoder");
-		names.put("covr","Cover artwork");
-		names.put("rtng","Advisory");
-		names.put("disk","Disk number");
-		names.put("ldes","Long description");
-		names.put("©ART","Artist");
-		names.put("hdvd","HD Video");
+
 	}
 
 	/**
@@ -41,6 +21,6 @@ public class AtomNameLookup {
 	 * @return the display name
 	 */
 	public String getDisplayName(String key) {
-		return names.get(key);
+		return MP4AtomKey.fromKey(key).getDisplayName();
 	}
 }
