da7e85c8c27bf21b4564527d108edbc9443a04bb
jp.stanford <jp.stanford@localhost>
2011-12-09 15:42:00 +0000

    Removed unneed JNA libs Better support for video images in the atomic parsley mp4 manager

--- tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/util/Platform.java
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/util/Platform.java
@@ -1,6 +1,20 @@
-package org.stanwood.media.jna;
-
-import com.sun.jna.Native;
+/*
+ *  Copyright (C) 2008-2011  John-Paul.Stanford <dev@stanwood.org.uk>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.stanwood.media.util;
 
 /** Provide simplified platform information. */
 @SuppressWarnings("all")
@@ -107,6 +121,7 @@ public final class Platform {
             || "amd64".equals(arch)) { //$NON-NLS-1$
             return true;
         }
-        return Native.POINTER_SIZE == 8;
+        throw new RuntimeException("Unable to workout bitness of JVM");
+//        return Native.POINTER_SIZE == 8;
     }
 }
