dc48620567e7520fea6a677f0c02b1a93d98cf2d
jp.stanford <jp.stanford@localhost>
2012-02-23 15:56:26 +0000

    Started to add support for debian packages

---
+++ tv-and-movies-meta-data-fetcher/build/debian/mediamanager.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mediamanager initscript
+# sourced by /etc/init.d/mediamanager
+# installed at /etc/default/mediamanager by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ tv-and-movies-meta-data-fetcher/build/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mediamanager):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mediamanager" command="/usr/bin/mediamanager"
