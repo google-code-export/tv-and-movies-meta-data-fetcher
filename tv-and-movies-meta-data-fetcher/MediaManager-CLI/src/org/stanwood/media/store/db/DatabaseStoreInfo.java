752423308117cb77203524d869c12084cc4d182a
jp.stanford <jp.stanford@localhost>
2012-06-03 09:38:13 +0000

    Registered new store

--- tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/store/db/DatabaseStoreInfo.java
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/store/db/DatabaseStoreInfo.java
@@ -1,14 +1,30 @@
 package org.stanwood.media.store.db;
 
+import org.stanwood.media.extensions.ExtensionException;
+import org.stanwood.media.extensions.ExtensionInfo;
+import org.stanwood.media.extensions.ExtensionType;
 import org.stanwood.media.extensions.ParameterType;
 
 /**
  * Extension information about the store {@link DatabaseStore}
  */
-public class DatabaseStoreInfo {
+public class DatabaseStoreInfo extends ExtensionInfo<DatabaseStore>  {
 
+	/** Parameter key for resourceId */
 	public static final ParameterType PARAM_DATABASE_RESOURCE_ID = new ParameterType("resourceId",String.class,false); //$NON-NLS-1$
 
 	private final static ParameterType PARAM_TYPES[] = new ParameterType[]{PARAM_DATABASE_RESOURCE_ID};
+
+	/**
+	 * The constructor
+	 */
+	public DatabaseStoreInfo() {
+		super(DatabaseStore.class.getName(),ExtensionType.STORE, PARAM_TYPES);
+	}
+
+	@Override
+	protected DatabaseStore createExtension() throws ExtensionException {
+		return new DatabaseStore();
+	}
 
 }
