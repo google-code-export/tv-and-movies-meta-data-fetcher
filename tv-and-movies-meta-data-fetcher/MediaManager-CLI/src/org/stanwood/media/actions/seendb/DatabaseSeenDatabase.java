9e14e765f40d6deab330bbf22523d4c44fdaf945
jp stanford <jp.stanford@gmail.com>
2012-06-22 13:09:50 +0000

    Added a seen DB that uses database resources to store it's contents

---
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/src/org/stanwood/media/actions/seendb/DatabaseSeenDatabase.java
@@ -0,0 +1,120 @@
+/*
+ *  Copyright (C) 2008-2011  John-Paul.Stanford <dev@stanwood.org.uk>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.stanwood.media.actions.seendb;
+
+import java.io.File;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Transaction;
+import org.hibernate.classic.Session;
+import org.stanwood.media.database.DBHelper;
+import org.stanwood.media.database.DatabaseException;
+import org.stanwood.media.progress.IProgressMonitor;
+import org.stanwood.media.setup.DBResource;
+
+public class DatabaseSeenDatabase implements ISeenDatabase{
+
+	private Session session;
+
+	public DatabaseSeenDatabase(DBResource resource) throws DatabaseException {
+		this.session = DBHelper.getInstance().getSession(resource);
+	}
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean isSeen(File mediaDirectory, File file) throws SeenDBException {
+		Transaction trans = session.beginTransaction();
+		try {
+			Query q = session.createQuery(" from SeenEntry where fileName = :path and lastModified = :modified"); //$NON-NLS-1$
+			q.setLong("modified", file.lastModified()); //$NON-NLS-1$
+			q.setString("path",file.getAbsolutePath()); //$NON-NLS-1$
+			List result = q.list();
+			if (result.size()>0) {
+				return true;
+			}
+			return false;
+		}
+		finally {
+			trans.commit();
+		}
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void markAsSeen(File mediaDirectory, File file) throws SeenDBException {
+		Transaction trans = session.beginTransaction();
+		long lastModified = file.lastModified();
+		String path = file.getAbsolutePath();
+		markAsSeen(mediaDirectory, lastModified, path);
+		trans.commit();
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void markAsSeen(File mediaDirectory,  long lastModified, String path) {
+		Query q = session.createQuery(" from SeenEntry where fileName = :path"); //$NON-NLS-1$
+		q.setString("path",path); //$NON-NLS-1$
+		List result = q.list();
+		SeenEntry entry;
+		if (result.size()==0) {
+			entry = new SeenEntry();
+			entry.setFileName(path);
+		}
+		else {
+			entry = (SeenEntry) result.get(0);
+		}
+		entry.setLastModified(lastModified);
+		session.saveOrUpdate(entry);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void write(IProgressMonitor parentMonitor) throws SeenDBException {
+		session.flush();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void read(IProgressMonitor progress) throws SeenDBException {
+
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void renamedFile(File mediaDirectory, File oldFile, File newFile)
+			throws SeenDBException {
+		Transaction trans = session.beginTransaction();
+		Query q = session.createQuery("update SeenEntry set fileName = :newFile where fileName = :oldFile"); //$NON-NLS-1$
+		q.setString("newFile",oldFile.getAbsolutePath()); //$NON-NLS-1$
+		q.setString("oldFile",newFile.getAbsolutePath()); //$NON-NLS-1$
+		q.executeUpdate();
+		trans.commit();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void removeFile(File mediaDirectory, File file)
+			throws SeenDBException {
+		Transaction trans = session.beginTransaction();
+		Query q = session.createQuery("delete from SeenEntry where fileName = :path"); //$NON-NLS-1$
+		q.setString("path",file.getAbsolutePath()); //$NON-NLS-1$
+		q.executeUpdate();
+		trans.commit();
+	}
+
+}
---
+++ tv-and-movies-meta-data-fetcher/MediaManager-CLI/tests/org/stanwood/media/actions/TestDatabaseSeenDatabase.java
@@ -0,0 +1,131 @@
+package org.stanwood.media.actions;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.stanwood.media.actions.seendb.DatabaseSeenDatabase;
+import org.stanwood.media.actions.seendb.FileSeenDatabase;
+import org.stanwood.media.actions.seendb.ISeenDatabase;
+import org.stanwood.media.progress.NullProgressMonitor;
+import org.stanwood.media.setup.DBResource;
+import org.stanwood.media.setup.SchemaCheck;
+import org.stanwood.media.util.FileHelper;
+
+/**
+ * This is used to test the class {@link FileSeenDatabase}
+ */
+@SuppressWarnings("nls")
+public class TestDatabaseSeenDatabase {
+
+	/**
+	 * Test that files can be marked as seen and checked
+	 * @throws Exception Thrown if their is a problem
+	 */
+	@Test
+	public void testDatabase() throws Exception {
+		File mediaDir = FileHelper.createTmpDir("config");
+		File dbFile = FileHelper.createTempFile("mediaInfo","db"); //$NON-NLS-1$
+		DBResource resource = createDBResource(dbFile);
+
+		try {
+			ISeenDatabase db = new DatabaseSeenDatabase(resource);
+
+			db.markAsSeen(mediaDir,createFile(mediaDir,"test1.avi"));
+			File test2 = createFile(mediaDir,"test2.avi");
+			db.markAsSeen(mediaDir,test2);
+			createFile(mediaDir,"test3.avi");
+			createFile(mediaDir,"test4.avi");
+
+			Assert.assertTrue(db.isSeen(mediaDir, new File(mediaDir,"test1.avi")));
+			Assert.assertTrue(db.isSeen(mediaDir, test2));
+			Assert.assertFalse(db.isSeen(mediaDir, new File(mediaDir,"test3.avi")));
+			Assert.assertFalse(db.isSeen(mediaDir, new File(mediaDir,"test4.avi")));
+
+			long orgTime = test2.lastModified();
+			if (!test2.setLastModified(orgTime+1000)) {
+				throw new IOException("Unable to set time");
+			}
+			Assert.assertTrue(orgTime!=test2.lastModified());
+
+			Assert.assertTrue(db.isSeen(mediaDir, new File(mediaDir,"test1.avi")));
+			Assert.assertFalse(db.isSeen(mediaDir, test2));
+			Assert.assertFalse(db.isSeen(mediaDir, new File(mediaDir,"test3.avi")));
+			Assert.assertFalse(db.isSeen(mediaDir, new File(mediaDir,"test4.avi")));
+		}
+		finally {
+			FileHelper.delete(dbFile);
+			FileHelper.delete(mediaDir);
+		}
+	}
+
+
+
+	/**
+	 * Used to test that reading and writing work correctly
+	 * @throws Exception Thrown if their are any problems
+	 */
+	@Test
+	public void testReadWrite() throws Exception {
+		File mediaDir = FileHelper.createTmpDir("config");
+		File dbFile = FileHelper.createTempFile("mediaInfo","db"); //$NON-NLS-1$
+		DBResource resource = createDBResource(dbFile);
+		try {
+			ISeenDatabase db =  new DatabaseSeenDatabase(resource);
+
+			File test1 = createFile(mediaDir,"test1.avi");
+			db.markAsSeen(mediaDir,test1);
+			File test2 = createFile(mediaDir,"test2.avi");
+			File test3 = createFile(mediaDir,"test3.avi");
+			File test4 =createFile(mediaDir,"test4.avi");
+
+			Assert.assertTrue(db.isSeen(mediaDir, new File(mediaDir,"test1.avi")));
+			long orgTime = test2.lastModified();
+			if (!test2.setLastModified(orgTime+1000)) {
+				throw new IOException("Unable to set time");
+			}
+			db.markAsSeen(mediaDir,test2);
+
+			Assert.assertTrue(orgTime!=test2.lastModified());
+			db.write(new NullProgressMonitor());
+
+			db =  new DatabaseSeenDatabase(resource);
+			Assert.assertTrue(db.isSeen(mediaDir, test1));
+			Assert.assertTrue(db.isSeen(mediaDir, test2));
+			Assert.assertFalse(db.isSeen(mediaDir, test3));
+			Assert.assertFalse(db.isSeen(mediaDir, test4));
+
+			db.read(new NullProgressMonitor());
+			Assert.assertTrue(db.isSeen(mediaDir, test1));
+			Assert.assertTrue(db.isSeen(mediaDir, test2));
+			Assert.assertFalse(db.isSeen(mediaDir, test3));
+			Assert.assertFalse(db.isSeen(mediaDir, test4));
+		}
+		finally {
+			FileHelper.delete(mediaDir);
+			FileHelper.delete(dbFile);
+		}
+	}
+
+	private File createFile(File mediaDir, String name) throws IOException {
+		File file = new File(mediaDir,name);
+		if (!file.createNewFile() && !file.exists()) {
+			throw new IOException("Unable to create file: " + file);
+		}
+		return file;
+	}
+
+	private DBResource createDBResource(File dbFile) {
+		DBResource resource = new DBResource();
+		resource.setDialect("org.hibernate.dialect.HSQLDialect"); //$NON-NLS-1$
+		resource.setUsername("sa"); //$NON-NLS-1$
+		resource.setPassword(""); //$NON-NLS-1$
+		resource.setUrl("jdbc:hsqldb:file:"+dbFile.getAbsolutePath()); //$NON-NLS-1$
+		resource.setResourceId("file-"+dbFile.getAbsolutePath()); //$NON-NLS-1$
+		resource.setSchemaCheck(SchemaCheck.UPDATE);
+		return resource;
+	}
+
+}
